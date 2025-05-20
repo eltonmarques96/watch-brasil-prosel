cb12983456584e4a3a514520f66937aa6da79858
Elton <elton1314@hotmail.com>
2025-05-19 23:44:32 -0300

    feat(backend): add Nest Project with CRUD of user and pino module for log

---
+++ backend/src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger } from 'nestjs-pino';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, { bufferLogs: true });
+  app.useLogger(app.get(Logger));
+  console.log(process.env.APP_PORT);
+  await app.listen(process.env.APP_PORT ?? 3000);
+}
+bootstrap();
